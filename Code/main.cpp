03ce09ae93186dfd69df3310cd2389096da7b2f2
kthvg5 <kthvg5@mst.edu>
2015-12-02 01:34:39 -0600

    added description of plan to main.cpp. Started on vertex.h

---
+++ Code/main.cpp
@@ -0,0 +1,40 @@
+
+/*
+FIRST THINGS FIRST, an explanation of what the plan is for when I'm working on this
+later. As with all great plans, this is subject to change (especially the names).
+
+notes to know before reading our steps:
+-we will be using three data structures called Path, Edge, and Vertex.
+-Our Dijkstra will be modified to ignore all edges with weight -1 and modify some stuff.
+-
+
+1) Modify our input slightly so that all the nodes with no edges starting from them have an edge back to themselves with wight -1
+    Normally I would say this is lowbrow, but it saves time and only has to work for one dataset. This project could be done without
+    this step, but it would take longer.
+2) Input our edges into our Edge DS from the input.txt file, storing them into an array (lets call it ReplaceableByMatrix, cuz it is).
+    Quicksort this alphabetically by the StartVertex.
+3) Make an array to hold the verteces (lets call it Vertical, because, reasons).
+    Populate it by going through ReplaceableByMatrix (RBM).
+    For the first edge of RBM, make a new Vertex with name =  RBM[0]StartVertex.
+    For each other edge i in RBM, if RBM[i].StartVertex doesnt equal RBM[i-1].StartVertex, make a new Vertex with name RBM[i].StartVertex.
+    These verteces should be in alphabetic order.
+    When making these verteces, update RBM.StartIndex
+4) Run through RBM getting the EndIndex (or whatever we end up calling it) using a binary search of Vertical
+5) Create a matrix for the edges (lets call it Edgey to describe how the class makes some students feel)
+    Populate it by going through RBM and coppieng each entry into the proper location (based on the StartIndex and EndIndex)
+    This is, in practice, just an adjacency matrix.
+    As it populates, it will also update the verices InDegree and OutDegree.
+6) Create a matrix of Path elements called Paths (because it's getting late and I'm out of good names).
+7) Okay,here is where you run the modified Dijkstra (or BFS). For each path found, our Dijkstra (or BFS) will:
+    a: Put the parent of the vertex added to the solution into the proper entry on Paths
+        (so if a path goes A -> B -> C, Paths[A][C] would contain B.)
+    b: Call a recursive algorithm that incriments all edges used.
+        (example above would go to Paths[A][C] and see B, incriment the edge from B to C, and then call
+            itself on Paths[A][B] and continue until it sees that it would be calling itself on Paths[A][A])
+    c: Update verious stats on the vertices based on if this is the Weighted/Unweighted Directed/Undirected run.
+8) Mark the most used edge as not open (there is a bool for this).
+    If there is a tie hightest centrality, mark all of them.
+9) Repeat 4x (so, five total itterations)
+
+At verious points through this we will be outputting some information so that we can make graphs out of it later.
+*/
---
+++ Code/vertex.h
@@ -0,0 +1,57 @@
+#ifndef VERTEX_H
+#define VERTEX_H
+using namespace std;
+
+#include <fstream>
+#include <string>>
+#include <iostream>
+
+class Vertex{
+public;
+/*
+Key to reading veriable names:
+DW = Directed Weighted
+UW = Undirected Weighted
+DU = Directed Unweighted
+UU = Undirected Unweighted
+LSP = Longest Shortest Path to START at the current vertex
+InDegree is the sum of the weights of all incoming edges (weight = 1 for unweighted)
+OutDegree is the sum of the weights of all outgoing edges (weight = 1 for unweighted)
+ClosenessCentrality is the average path length to all vertices from the chosen vertex
+
+PathLength is a little tricky and requires knowledge of how we are storing these to
+understand. Basically, all verteces will be stored in an array and sorted alphabetically.
+PathLength[i] stores the weight of the path from this vertex to the vertex in with an index
+of i in the array of all vertices.
+*/
+    string name;
+    int index; //yes, this can be gotten, but we only have a week and this is easier.
+
+    //statistics on Vertex in a Directed Weighted graph
+    int DW_InDegree;
+    int DW_OutDegree;
+    int DW_LSP_Weight; //Stores the weight of the LSP to start from this vertex
+    string DW_LSP_EndNode;//Stores the endnode of that path
+    int DW_PathLength(547);
+
+    //statistics on Undirected Weighted
+    int UW_LSP_Weight;
+    string UW_LSP_EndNode;
+    int UW_PathLength(547);
+
+    //statistics on the Directed Unweighted graph
+    int DU_InDegree;
+    int DU_OutDegree;
+    int DU_LSP_Weight;
+    string DU_LSP_EndNode;
+    int DU_PathLength(547);
+    int DU_ClosenessCentrality;
+
+    //statistics on Vertex in an Undirected Unweighted graph
+    int UU_LSP_Weight; //again, longest shortest path that STARTS here
+    string UU_LSP_EndNode;
+    int UU_PathLength(547);
+    int UU_ClosenessCentrality;
+};
+
+#endif // VERTEX_H
