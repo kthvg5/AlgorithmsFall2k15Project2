d6b1d9230a25d39bd10d17085906c6f02cb710b5
kthvg5 <kthvg5@mst.edu>
2015-12-02 02:21:21 -0600

    added Path class. Again, sans functions for now.

---
+++ Code/path.h
@@ -0,0 +1,15 @@
+#ifndef PATH_H
+#define PATH_H
+class Path{
+/*
+Key to reading veriable names:
+DW = Directed Weighted
+UW = Undirected Weighted
+DU = Directed Unweighted
+UU = Undirected Unweighted
+*/
+public;
+    int DW, UW, DU, UU;
+};
+
+#endif // PATH_H
