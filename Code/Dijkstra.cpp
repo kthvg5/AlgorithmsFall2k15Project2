fc17d0255c218a20e0ade848338622f4650b8a98
kthvg5 <kthvg5@mst.edu>
2015-12-08 22:55:21 -0600

    added Undirected dijkstra

--- Code/Dijkstra.cpp
+++ Code/Dijkstra.cpp
@@ -1,6 +1,7 @@
 #include "Dijkstra.h"
 
-void DW_dijkstra(Path WayHome[][], Vertex nodes[], Edge Matrix[][], int num_verts)
+//Directed Weighted
+void UW_dijkstra(Path WayHome[][], Vertex nodes[], Edge Matrix[][], int num_verts)
 {
     int bestFrom, bestTo, bestWeight = INT_MAX;
     for(int i = 0; i < num_verts; i++)
@@ -8,17 +9,17 @@ void DW_dijkstra(Path WayHome[][], Vertex nodes[], Edge Matrix[][], int num_vert
         addToSolution(nodes[i], Matrix, num_verts);
         for(int x = 1; x < num_verts; x++)
         {
-            for(int from = 0; from < num_verts; from++)
-            {
-                if(nodes[from].solution == true)
-                {
+            for(int from = 0; from < num_verts; from++)   //this goes through every possible edge (yes,
+            {   //even the ones that aren't there) and finds which one is the best adition to our solution.
+                if(nodes[from].solution == true) //this should do a lot of
+                {       //optimizing since it cuts out half of our looping
                     for(int to = 0; to < num_verts; to++)
                     {
-                        if(Matrix[from][to].Dweight !=-1)
-                        {
-                            if(Matrix[from][to].Dweight + Path[from][to].weight < bestWeight
-                            && Matrix[from][to].DW_Solution == false
-                            && Matrix[from][to].DW_Frontier == true)
+                        if(Matrix[from][to].Dweight !=-1)   //since the agerage node has
+                        {   //like four edges going out of it, this should also cut some time. (note, weight of -1 indicates no edge)
+                            if(Matrix[from][to].Dweight + Path[from][to].weight < bestWeight//checks if weight is better than current
+                            && Matrix[from][to].DW_Solution == false   //makes sure this isnt an edge we already got
+                            && Matrix[from][to].DW_Frontier == true)   //makes sure this edge is one we are able to get
                             {
                                 bestFrom = from;
                                 bestTo = to;
@@ -27,9 +28,8 @@ void DW_dijkstra(Path WayHome[][], Vertex nodes[], Edge Matrix[][], int num_vert
                     }
                 }
             }
-            addToSolution(WayHome, nodes[Matrix[bestFrom][bestTo].index_End],
-            Matrix, num_verts);
-            DW_GoHome(Matrix, i, Matrix[bestFrom][bestTo].index_End, WayHome);
+            addToSolution(WayHome, nodes[Matrix[bestFrom][bestTo].index_End], Matrix, num_verts);   //adds "Best" node to solution
+            DW_GoHome(Matrix, i, Matrix[bestFrom][bestTo].index_End, WayHome);   //paths itself back home
         }
         reset(Matrix, nodes, num_verts);
     }
@@ -59,11 +59,61 @@ void DW_reset(Edge Matrix[][], Vertex Nodes[], int num_verts){
     return;
 }
 
+//Undirected Weighted
+void UW_dijkstra(Path WayHome[][], Vertex nodes[], Edge Matrix[][], int num_verts)
+{
+    int bestFrom, bestTo, bestWeight = INT_MAX;
+    for(int i = 0; i < num_verts; i++)
+    {
+        addToSolution(nodes[i], Matrix, num_verts);
+        for(int x = 1; x < num_verts; x++)
+        {
+            for(int from = 0; from < num_verts; from++)   //this goes through every possible edge (yes,
+            {   //even the ones that aren't there) and finds which one is the best adition to our solution.
+                if(nodes[from].solution == true) //this should do a lot of
+                {       //optimizing since it cuts out half of our looping
+                    for(int to = 0; to < num_verts; to++)
+                    {
+                        if(Matrix[from][to].Uweight !=-1)   //since the agerage node has
+                        {   //like four edges going out of it, this should also cut some time. (note, weight of -1 indicates no edge)
+                            if(Matrix[from][to].Uweight + Path[from][to].weight < bestWeight//checks if weight is better than current
+                            && Matrix[from][to].UW_Solution == false   //makes sure this isnt an edge we already got
+                            && Matrix[from][to].UW_Frontier == true)   //makes sure this edge is one we are able to get
+                            {
+                                bestFrom = from;
+                                bestTo = to;
+                            }
+                        }
+                    }
+                }
+            }
+            addToSolution(WayHome, nodes[Matrix[bestFrom][bestTo].index_End], Matrix, num_verts);   //adds "Best" node to solution
+            UW_GoHome(Matrix, i, Matrix[bestFrom][bestTo].index_End, WayHome);   //paths itself back home
+        }
+        reset(Matrix, nodes, num_verts);
+    }
+    return;
+}
 
+void UW_addToSolution(Vertex Node, Edge Matrix[][], int num_verts, int from){
+    Node.UW_Solution = true;
+    for (int i = 0; i < num_verts; i++)
+    {
+        Matrix[i][Node.index].UW_Solution = True;
+        Matrix[Node.index][i].UW_Frontier = True;
+    }
+    return;
+}
 
-
-
-
-
-
-
+void UW_reset(Edge Matrix[][], Vertex Nodes[], int num_verts){
+    for(int i = 0; i < num_verts; i++)
+    {
+        Nodes[i].UW_Solution = false;
+        for(int x = 0; x < num_verts; x++)
+        {
+            Matrix[i][x].UW_Frontier = false;
+            Matrix[i][x].UW_Solution = false;
+        }
+    }
+    return;
+}
--- Code/Dijkstra.h
+++ Code/Dijkstra.h
@@ -14,11 +14,14 @@ using namespace std;
 //to run our implimentation of Dijkstra.
 
 void DW_dijkstra(Path WayHome[][], Vertex nodes[], Edge Matrix[][], int num_verts);
+void UW_dijkstra(Path WayHome[][], Vertex nodes[], Edge Matrix[][], int num_verts);
 
 //adds a node and it's incoming edges to the solution and it's outgoing
 //edges to the frontier. There are probably better ways to do this, but
 //we are using bools.
 void DW_addToSolution(Vertex Node, Edge Matrix[][], int num_verts);
+void UW_addToSolution(Vertex Node, Edge Matrix[][], int num_verts);
 
 //sets all frontier and solution bools to false
 void DW_reset(Edge Matrix[][], Vertex Nodes[], int num_verts);
+void UW_reset(Edge Matrix[][], Vertex Nodes[], int num_verts);
