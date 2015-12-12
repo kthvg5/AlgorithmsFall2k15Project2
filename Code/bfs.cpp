83eb43e45c856bff8d22cb56b800c424aabcd753
John Niemeyer <jjnb78@mst.edu>
2015-12-12 13:34:57 -0600

    Update bfs.cpp

--- Code/bfs.cpp
+++ Code/bfs.cpp
@@ -20,12 +20,12 @@ void directed_BFS(Vertex array[], Edge d_edge[][vertices], Path d_path[][vertice
         //in the correct order
         while(!path.empty()){
             for(int s = 0; s < vertices; s++){
-                if(d_edge[from][s].Dweight != -1 && d_edge[from][s].UU_removed != true && array[s].DU_Found == false){
+                if(d_edge[from][s].Dweight != -1 && d_edge[from][s].UU_Removed != true && array[s].DU_Found == false){
                     parent = path.front();
                     path.push(array[s]);
-                    Vertex[s].DU_Found = true;
+                    array[s].DU_Found = true;
                     d_path[x][s].DU_Parent = from;
-                    d_path[x][s].DU_Weight = d_path[from][s] + 1;
+                    d_path[x][s].DU_Weight = d_path[from][s].DU_Weight + 1;
                     d_path[x][s].DU_GoHome(Edge d_edge[][vertices], int from, int s, Path d_path[][vertices], int parent);
                 }
             }
@@ -59,9 +59,9 @@ void undirected_BFS(Vertex array[], Edge d_edge[][vertices], Path d_path[][verti
                 if(d_edge[from][s].Uweight != -1 && d_edge[from][s].UU_removed != true && array[s].DU_Found == false){
                     parent = path.front();
                     path.push(array[s]);
-                    Vertex[s].UU_Found = true;
+                    array[s].UU_Found = true;
                     d_path[x][s].UU_Parent = from;
-                    d_path[x][s].UU_Weight = d_path[from][s] + 1;
+                    d_path[x][s].UU_Weight = d_path[from][s].DU_Weight + 1;
                     d_path[x][s].UU_GoHome(Edge d_edge[][vertices], int from, int s, Path d_path[][vertices], int parent);
                 }
             }
