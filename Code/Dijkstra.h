b4468f00cf6a538c4e06ba7b9a1e0f457188ddf8
John Niemeyer <jjnb78@mst.edu>
2015-12-12 13:16:51 -0600

    Update Dijkstra.h

--- Code/Dijkstra.h
+++ Code/Dijkstra.h
@@ -13,15 +13,15 @@ using namespace std;
 //This file contains the function declarations for the functions required
 //to run our implimentation of Dijkstra.
 
-void DW_dijkstra(Path WayHome[][], Vertex nodes[], Edge Matrix[][], int num_verts);
-void UW_dijkstra(Path WayHome[][], Vertex nodes[], Edge Matrix[][], int num_verts);
+void DW_dijkstra(Path WayHome[][547], Vertex nodes[], Edge Matrix[][547], int num_verts);
+void UW_dijkstra(Path WayHome[][547], Vertex nodes[], Edge Matrix[][547], int num_verts);
 
 //adds a node and it's incoming edges to the solution and it's outgoing
 //edges to the frontier. There are probably better ways to do this, but
 //we are using bools.
-void DW_addToSolution(Vertex Node, Edge Matrix[][], int num_verts);
-void UW_addToSolution(Vertex Node, Edge Matrix[][], int num_verts);
+void DW_addToSolution(Vertex Node, Edge Matrix[][547], int num_verts);
+void UW_addToSolution(Vertex Node, Edge Matrix[][547], int num_verts);
 
 //sets all frontier and solution bools to false
-void DW_reset(Edge Matrix[][], Vertex Nodes[], int num_verts);
-void UW_reset(Edge Matrix[][], Vertex Nodes[], int num_verts);
+void DW_reset(Edge Matrix[][547], Vertex Nodes[], int num_verts);
+void UW_reset(Edge Matrix[][547], Vertex Nodes[], int num_verts);
