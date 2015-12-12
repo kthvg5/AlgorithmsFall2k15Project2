e151ceb0d87f04be309cd46a724d6006a39e14f4
John Niemeyer <jjnb78@mst.edu>
2015-12-12 13:42:06 -0600

    Update bfs.cpp

--- Code/bfs.cpp
+++ Code/bfs.cpp
@@ -43,20 +43,20 @@ void undirected_BFS(Vertex array[], Edge d_edge[][vertices], Path d_path[][verti
     int parent;
 
     //Iterate through the entire vertex array and find all BFS
-    for(int x = 0, x < vertices; x++){
+    for(int x = 0; x < vertices; x++){
         int from = x;
 
         //Creating the queue, place starting element into the queue
         queue<Vertex> path;
         path.push(array[x]); //queue
 
-        array[x].UU_found = true;
+        array[x].UU_Found = true;
 
         //place the elements connected to the starting element into the queue and into the list
         //in the correct order
         while(!path.empty()){
             for(int s = 0; s < vertices; s++){
-                if(d_edge[from][s].Uweight != -1 && d_edge[from][s].UU_removed != true && array[s].DU_Found == false){
+                if(d_edge[from][s].Uweight != -1 && d_edge[from][s].UU_Removed != true && array[s].DU_Found == false){
                     parent = path.front();
                     path.push(array[s]);
                     array[s].UU_Found = true;
