45fa93c194b26a9a20f1cc3d441cd0193967047a
John Niemeyer <jjnb78@mst.edu>
2015-12-13 13:03:06 -0600

    Update bfs.cpp

--- Code/bfs.cpp
+++ Code/bfs.cpp
@@ -5,28 +5,35 @@ const int vertices = 547;
 void directed_BFS(Vertex array[], Edge d_edge[][vertices], Path d_path[][vertices])
 {
     Vertex parent;
+    queue<Vertex> path;
 
     //Iterate through the entire vertex array and find all BFS
     for(int x = 0; x < vertices; x++){
         int from = x;
-
+        cout << "hello" << endl;
         //Creating the queue, place starting element into the queue
-        queue<Vertex> path;
         path.push(array[x]); //queue
+        cout << "this isnt the problem" << endl;
         parent = path.front();
+        cout << "prolly not the prolem" << endl;
         array[x].DU_Found = true;
+        cout << "possible" << endl;
         d_path[x][x].DU_Parent = x;
         d_path[x][x].DU_Weight = 0;
         //place the elements connected to the starting element into the queue and into the list
         //in the correct order
-        while(!path.empty()){
+        while(!path.empty() && parent.index !=-1){
+            cout << "do we get back in here?" << endl;
             for(int s = 0; s < vertices; s++){
-                if(d_edge[parent.index][s].Dweight != -1 && d_edge[from][s].DU_Removed != true && array[s].DU_Found == false){
+                    cout << "s = " << s << endl;
+                    cout << "parent = " << parent.index << endl;
+                if(d_edge[parent.index][s].Dweight != -1 && d_edge[parent.index][s].DU_Removed != true && array[s].DU_Found != true){
+                    cout << "im back" << endl;
                     parent = path.front();
                     path.push(array[s]);
                     array[s].DU_Found = true;
-                    d_path[x][s].DU_Parent = from;
-                    d_path[x][s].DU_Weight = d_path[from][s].DU_Weight + 1;
+                    d_path[x][s].DU_Parent = parent.index;
+                    d_path[x][s].DU_Weight = d_path[x][s].DU_Weight + 1;
                     DU_GoHome(d_edge, from, s, d_path, parent);
                 }
             }
@@ -54,9 +61,10 @@ void undirected_BFS(Vertex array[], Edge d_edge[][vertices], Path d_path[][verti
         array[x].UU_Found = true;
         d_path[x][x].UU_Parent = x;
         d_path[x][x].UU_Weight = 0;
+        cout << "x = " << x << endl;
         //place the elements connected to the starting element into the queue and into the list
         //in the correct order
-        while(!path.empty()){
+        while(!path.empty()&& parent.index !=-1){
             for(int s = 0; s < vertices; s++){
                 if(d_edge[from][s].Uweight != -1 && d_edge[from][s].UU_Removed != true && array[s].UU_Found == false){
                     parent = path.front();
